970bfe2ecc741154555337f634861a5c920d4456
Prathamesh <prathprabhu@gmail.com>
2015-06-22 12:23:37 +0530

    Setup backend skeleton

---
+++ server.js
@@ -0,0 +1,25 @@
+
+var express = require('express');  //node module for express server
+
+var app =express(); //initialize express
+
+app.set('views',__dirname + '/server/views');
+
+console.log("This is the path " + __dirname);
+
+//app use is used to use middleware
+app.use(express.static('public')); //static route handling
+
+app.set('view engine','jade');
+
+// routes   req=request res = response ..It stimulates a http request..Both are global objects
+
+app.get('/', function(req,res){
+
+	res.render('index');
+})
+
+app.listen(3020); //listen at specific port
+
+console.log("Express server is listening at port 3020");
+
